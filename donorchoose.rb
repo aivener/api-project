c33da0a82dfabfc36defc9a1e920be1262fd7e5a
Allie Ivener <adi2hf@virginia.edu>
2014-06-22 20:37:28 +0200

    donor commit with output

--- donorchoose.rb
+++ donorchoose.rb
@@ -3,33 +3,59 @@ require 'rest-client'
 
 puts "Hello! Welcome to donorschoose.org!"
 
-#would you like to search by subject? (y/n) if y, give possibilities
-#or, would you like to search by community (y/n)?
-#or, would you like to search by resource type?
+offset = 0
 
-puts "Would you like to search by subject, state, or resource type?"
+while true
 
-choice = gets.strip
+	puts "Would you like to search by subject, state, poverty level, or resource type?"
 
-url = "http://api.donorschoose.org/common/json_feed.html?APIKey=DONORSCHOOSE"
+	choice = gets.strip
 
-if choice == "subject"
-	puts "What subject would you like to search for?"
-	subject = gets.strip
-	response = RestClient.get("http://api.donorschoose.org/common/json_feed.html?subject=#{subject}&APIKey=DONORSCHOOSE")
+	if choice == "subject"
+		puts "What subject would you like to search for?"
+		subject = gets.strip
+		puts "Loading..."
+		response = RestClient.get("http://api.donorschoose.org/common/json_feed.html?max=10&index=#{offset}&subject=#{subject}&APIKey=DONORSCHOOSE")
+	elsif choice == "state"
+		puts "Please enter the state initials."
+		state = gets.strip
+		puts "Loading..."
+		response = RestClient.get("http://api.donorschoose.org/common/json_feed.html?max=10&index=#{offset}&state=#{state}&APIKey=DONORSCHOOSE")
+	elsif choice == "resource type"
+		puts "Enter the number for your desired resource type.\n1 Books\n2 Technology\n3 Supplies\n4 Trips\n5 Visitors\n6 Other"
+		resource_type = gets.strip
+		puts "Loading..."
+		response = RestClient.get("http://api.donorschoose.org/common/json_feed.html?max=10&index=#{offset}&proposalType=#{resource_type}&APIKey=DONORSCHOOSE")
+	elsif choice == "poverty level"
+		puts "Loading..."
+		response = RestClient.get("http://api.donorschoose.org/common/json_feed.html?max=10&index=#{offset}&highLevelPovery=true&APIKey=DONORSCHOOSE")
+	else
+		puts "Please enter a valid search term."
+	end
+
+	next unless response
+
 	parsed_response = JSON.parse(response)
-elsif choice == "community"
-	puts "Please enter the state initials."
-	state = gets.strip
-	response = RestClient.get("http://api.donorschoose.org/common/json_feed.html?state=#{state}&APIKey=DONORSCHOOSE")
-	parsed_response = JSON.parse(response)
-elsif choice == "resource type"
-	puts "Enter the number for your desired resource type.\n1 Books\n2 Technology\n3 Supplies\n4 Trips\n5 Visitors\n Other"
-	resource_type = gets.strip
-	response = RestClient.get("http://api.donorschoose.org/common/json_feed.html?proposalType=#{resource_type}&APIKey=DONORSCHOOSE")
-	parsed_response = JSON.parse(response)
-else
-	puts "Please enter a valid search term."
+
+	results = parsed_response["proposals"]
+	#array of hashes
+
+	results.each_with_index do |proposal, index|
+		if offset < results.length - 1
+			title = results[index + offset]["title"]
+			description = results[index + offset]["shortDescription"]
+			fund_url = results[index + offset]["fundURL"]
+			puts "#{offset + index + 1}. (#{title}): #{description}, #{fund_url}"
+		else
+			puts "For more results, check out www.donorschoose.org or try again with a different search parameter."
+			break
+		end
+	end
+
+	puts "Would you like to search again? (Y/N)"
+
+	break unless gets.strip == "Y"
+
+	offset += 10
+
 end
-
-#do stuff with parsed_response
